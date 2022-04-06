ff8eef80ac07fbd51314912176a69ec0a344b02b
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-04-06 14:02:23 +0000

    [Update]indexページのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id]) 
+    book.destroy
+    redirect_to '/books'
   end
 end
