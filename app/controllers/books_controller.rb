d0b24bb11b234a94a7227d6b5e28a0a237a168d4
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-04-07 02:52:48 +0000

    [Update]indexページ投稿・リンク編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,13 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def show
@@ -16,4 +23,15 @@ class BooksController < ApplicationController
     book.destroy
     redirect_to '/books'
   end
+  
+  
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
+
