47b538993b99c820ab891aa5b83ebce3fc375a57
Emiri Maegawa <101238309+mgwem@users.noreply.github.com>
2022-04-07 14:29:41 +0000

    [Update]update機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.save
@@ -17,18 +17,24 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
-    book = Book.find(params[:id]) 
+    book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
-  
-  
-  
+
+
+
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
